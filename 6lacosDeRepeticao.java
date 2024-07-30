ba4155d51429a9bcfc03b356c369128d4498766e
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2024-07-30 16:16:51 -0300

    Create 6lacosDeRepeticao.java

---
+++ 6lacosDeRepeticao.java
@@ -0,0 +1,18 @@
+public class ContagemRegressiva {
+    public static void main(String[] args){
+        int i = 99;
+        
+        while(i >= 66){
+            System.out.println(i);
+            i--;
+        }
+        
+        do{
+            System.out.println(i);
+            i--;
+        } while(i >= 33);
+        
+        for(i = 32; i >= 0; i--)
+            System.out.println(i);
+    }
+}
