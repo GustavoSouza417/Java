95f495be521cea6f8461132b11180d7e9aea6393
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2024-07-31 14:45:31 -0300

    Update A10tryCatchComExcecaoGenerica.java

--- A10tryCatchComExcecaoGenerica.java
+++ A10tryCatchComExcecaoGenerica.java
@@ -1,4 +1,4 @@
-//classe que recebe erros genéricos de exceção (ela não trata erros graves)
+//classe que trata de exceções genéricas
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -12,15 +12,14 @@ public class EntradaDeDadosValida2 {
                 int numero = scanner.nextInt();
                 break;
             }
-            //o "Exception" é a classe que trata de erros genéricos e ela faz instância em "e"
             catch(Exception e){
                 System.out.print("Ocorreu algum erro durante a execução da aplicação. Por favor, tente novamente.\n\n");
                 scanner.next();
             }
-            
-            //esse Exception consegue captar vários erros, como de outros tipos de dados (textos, números de ponto flutuante e etc.)
+
+            //esse Exception consegue capturar qualquer exceção        
             //de qualquer forma, sempre que possível, é melhor usar exceções específicas do que exceções genéricas
-            //exceções genéricas só devem ser usadas para erros muito inesperados
+            //exceções genéricas só devem ser usadas para garantir que não ocorram erros inesperados
         }
         
         scanner.close();
