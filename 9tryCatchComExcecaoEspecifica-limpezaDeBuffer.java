e681470e7e5986792b96dfb3a714d17dad936e76
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2024-07-31 14:41:08 -0300

    Update 9tryCatchComExcecaoEspecifica-limpezaDeBuffer.java

--- 9tryCatchComExcecaoEspecifica-limpezaDeBuffer.java
+++ 9tryCatchComExcecaoEspecifica-limpezaDeBuffer.java
@@ -12,7 +12,7 @@ public class EntradaDeDadosValida1 {
                 float dividendo = scanner.nextFloat();
                 break;
             }
-            //este "e" é uma instância desta classe de erro (ele pode ter outros nomes, mas "e" é o padrão)
+            //este "e" é uma instância desta classe de exceção (ele pode ter outros nomes, mas "e" é o padrão)
             catch(InputMismatchException e){
                 System.out.print("O tipo de dado digitado não é numérico!\n\n");
                 scanner.next(); //limpa a entrada anterior (sempre limpe o buffer após uma entrada inválida)
