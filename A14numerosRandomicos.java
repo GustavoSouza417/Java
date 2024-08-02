0b35f9d0bd6a719540cf818e2fca00ef91d9ebdc
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2024-08-02 12:52:17 -0300

    Update A14numerosRandomicos.java

--- A14numerosRandomicos.java
+++ A14numerosRandomicos.java
@@ -6,19 +6,18 @@ public class SpamNumerico {
         
         int numero;
         
-        //Random
-            System.out.print("Random:\n");
-            
-            //gera um número grande e sem nenhuma limitação
-            numero = random.nextInt();
-            System.out.printf("  1) %d\n", numero);
-            
-            //gera um número aleatório entre zero e trinta
-            numero = random.nextInt(30);
-            System.out.printf("  2) %d\n", numero);
-            
-            //você pode usar outros tipos de dados (float, double, bool e etc.) para ter outros resultados 
-            System.out.print("-------------------------------\n\n");
-        //
+        //gera um número grande e sem nenhuma limitação
+        numero = random.nextInt();
+        System.out.printf("  1) %d\n", numero);
+        
+        //gera um número aleatório entre zero e trinta
+        numero = random.nextInt(30);
+        System.out.printf("  2) %d\n", numero);
+        
+        //você pode usar outros tipos de dados (float, double, bool e etc.) para ter outros resultados
+        
+        //também é possível utilizar várias outras classes para a geração de números randômicos
+        //cada uma delas é mais apropriada para um tipo específico de contexto
+        //alguns exemplos delas incluem as classes "Math", "SplittableRandom", "SecureRandom", "ThreadLocalRandom" e afins
     }
 }
